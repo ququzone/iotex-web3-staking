93b9929b969bb4918e39cec71466c8292f0383b0
ququzone <xueping.yang@gmail.com>
2023-03-11 08:32:51 +0800

    add rewarding action

---
+++ src/rewarding-deposit.ts
@@ -0,0 +1,32 @@
+import * as ethers from "ethers"
+import * as dotenv from "dotenv"
+
+import * as rewardingABI from "./rewarding.json"
+
+dotenv.config()
+
+async function main() {
+    const provider = new ethers.providers.JsonRpcProvider("https://babel-api.testnet.iotex.io")
+
+    const privateKey = process.env.PRIVATE_KEY
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const wallet = new ethers.Wallet(privateKey!, provider)
+
+    const rewarding = new ethers.Contract(
+        "0xa576c141e5659137ddda4223d209d4744b2106be",
+        rewardingABI.abi,
+        wallet
+    )
+
+    console.log(wallet.address)
+
+    const tx = await rewarding.deposit(ethers.utils.parseEther("1.0"), [])
+    console.log(`deposit rewarding with tx ${tx.hash}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
